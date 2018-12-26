431848303804b56aa2055433092d4bcc67c47cd2
tlawo <tlawo@LAPTOP-ICSS4D7B>
2018-12-26 20:02:32 +0900

    test

--- src/practice01/Prob1.java
+++ src/practice01/Prob1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
-		
+		System.out.println("숫자를 ㅇ;ㅂ략 핫[요");
 		
 		scanner.close();
 	}
